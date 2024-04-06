9135b5066a68a83fb72e0429440b2afd4299e651
Debbie <debz_scott@icloud.com>
2024-04-06 22:14:45 +0100

    added basic calculator functions

---
+++ index.js
@@ -0,0 +1,47 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let numberOperator;
+
+const add = function(a, b) {
+    console.log(a + b);
+  };
+  
+
+
+  const subtract = function(a, b) {
+      return a - b;
+  };
+
+  
+   
+const multiply = function(a, b) {
+    console.log(a * b)
+};
+
+
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+
+const operate = function(initNumber, oper, lastNumber) {
+
+    
+
+    if (oper == "plus") {
+        add(initNumber, lastNumber)
+    }
+    else if (oper == "times") {
+        multiply(initNumber, lastNumber)
+    }
+    else if (oper == "minus") {
+        subtract(initNumber, lastNumber)
+    }
+    else if (oper == "share") {
+        divide(initNumber, lastNumber)
+    }
+
+  }
+
+  operate(23, "times", 24)
