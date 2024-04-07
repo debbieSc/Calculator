3c37386ed2ebf0822d4df3cd820f7e8b43c7267c
Debbie <debz_scott@icloud.com>
2024-04-07 02:21:21 +0100

    calculator display changes on button push

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let numberOperator;
+let array = [];
+let secondArray = [];
+let display = document.querySelector(".display")
+
+
 
 const add = function(a, b) {
     console.log(a + b);
@@ -44,4 +49,27 @@ const operate = function(initNumber, oper, lastNumber) {
 
   }
 
- 
+  
+  const buttonChoice = document.querySelectorAll(".buttonChoice");
+
+
+    buttonChoice.forEach(button => button.addEventListener("click", () => {
+
+        if (button.id == "ac") {
+            display.textContent = 0
+        }
+        else if (display.textContent == 0){
+            display.textContent = button.textContent
+        } else {
+
+        
+
+        display.textContent += button.textContent
+
+        }
+
+    
+    }))
+  
+
+   
